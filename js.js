1799ab35ce56c79f0bccecf089017f742cd8f02e
newnkgame <100308608+newnkgame@users.noreply.github.com>
2022-04-05 10:35:50 -0400

    Update js.js

--- js.js
+++ js.js
@@ -5,4 +5,4 @@ var front = 'https://ultra';
 var end = '.womginx.repl.co/service/hvtrs8%2F-wuw%2Csrovidy%2Ccmm-uq%2F';
 var el = document.getElementById('ifrm');
 
-el.src = front+num+end;
+window.location.replace(front+num+end);
