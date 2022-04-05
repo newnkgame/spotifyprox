dbda67c5646b5cffbf2a065433316acd23630fc2
newnkgame <100308608+newnkgame@users.noreply.github.com>
2022-04-05 10:26:01 -0400

    Update js.js

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ var stuff = '';
 var num = prompt("Enter Number", stuff);
 
 var front = 'https://ultra';
-var end = '.womginx.repl.co/service/hvtrs8%2F-orel.qpmtkf%7B.aoo%2F%3Dnf%3D3';
+var end = '.womginx.repl.co/service/hvtrs8%2F-wuw%2Csrovidy%2Ccmm-uq%2F';
 var el = document.getElementById('ifrm');
 
 el.src = front+num+end;
